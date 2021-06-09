473ded274fe99cab316df17262088e2dbb37bbed
Skylar Gering <sgering@g.hmc.edu>
2021-06-09 15:52:50 -0700

    script to match acoustic and CTD files were data collection overlaps

--- plotEchograms.py
+++ plotEchograms.py
@@ -3,10 +3,70 @@ from matplotlib.pyplot import figure, show, subplots_adjust, get_cmap
 from echolab2.processing import processed_data, line
 from echolab2.plotting.matplotlib import echogram
 from echolab2.instruments import EK60
-
-rawfiles = ['Volumes/GeringSSD/GU1905/EK60/GU19_05-D20191015-T112548.raw']
+# EK60      '/Volumes/GeringSSD/D20090916-T135430.raw'
+# EK80      '/Volumes/GeringSSD/GU1905_Acoustic/EK60/GU19_05-D20191015-T112548.raw'
+rawfiles = ['/Volumes/GeringSSD/D20090916-T135430.raw']
 
 fig = figure()
 # Set some properties for the sub plot layout.
 subplots_adjust(left=0.11, bottom=0.1, right=0.98, top=.93, wspace=None,
                 hspace=0.9)
+
+ek60 = EK60.EK60()
+ek60.read_raw(rawfiles)
+print(ek60)
+
+raw_data_38 = ek60.get_channel_data(frequencies=38000)
+raw_38 = raw_data_38[38000][0]
+
+ax_1 = fig.add_subplot(3, 1, 1)
+echogram_1 = echogram.Echogram(ax_1, raw_38, 'power')
+
+# Playing around
+echogram_1.add_colorbar(fig)
+
+ax_1.set_title("Power as stored in raw_data object")
+
+processed_power = raw_38.get_power()
+print(processed_power)
+
+ax_2 = fig.add_subplot(3, 1, 2)
+echogram_2 = echogram.Echogram(ax_2, processed_power)
+ax_2.set_title("Power data in time order")
+
+cal_obj = raw_38.get_calibration()
+Sv = raw_38.get_Sv(calibation=cal_obj)
+print(Sv)
+
+ax_3 = fig.add_subplot(3, 1, 3)
+echogram_3 = echogram.Echogram(ax_3, Sv, threshold=[-70,-34])
+ax_3.set_title("Sv data in time order")
+show()
+
+# Create another matplotlib figure.
+fig = figure()
+# Set some properties for the sub plot layout.
+subplots_adjust(left=0.1, bottom=0.1, right=0.98, top=.93, wspace=None,
+                hspace=0.5)
+
+angle_cmap = get_cmap('plasma')
+
+# Now request angles data in time order.
+angles_along, angles_athwart = raw_38.get_physical_angles()
+print(angles_along)
+print(angles_athwart)
+
+# Create another axis.
+ax_1 = fig.add_subplot(2, 1, 1)
+# Create an echogram which will display on our newly created axis.
+echogram_3 = echogram.Echogram(ax_1, angles_along, cmap=angle_cmap)
+ax_1.set_title("angles_alongship data in time order")
+
+# Create another axis.
+ax_2 = fig.add_subplot(2, 1, 2)
+# Create an echogram which will display on our newly created axis.
+echogram_3 = echogram.Echogram(ax_2, angles_athwart, cmap=angle_cmap)
+ax_2.set_title("angles_athwartship data in time order")
+
+# Show our figure.
+show()
--- plot_CTD_data.py
+++ plot_CTD_data.py
@@ -5,25 +5,45 @@
 # Skylar Gering 06/04/21
 ###############################################
 
-from datetime import datetime, timedelta
+from datetime import datetime
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import os
 
-def plotDepth(x, y, filepath):
+def evl_list(ctd_list):
+    """
+    From a CTDtoELV.list makes a list of .evl files that should be created 
+    if SeaBird-wTime-toEchoviewLine.py was run
+    Input: file path to CTDtoELV.list
+    Output: list of corresponding .evl filenames
+    """
+    with open(ctd_list, 'r') as infile:
+        ctd_files = infile.readlines() # read in each CDT file name and the toffset
+    
+    evl_files = []
+    for ctd in ctd_files:
+        # Get the filename and replace .asc with .evl - you now have all .evl filenames
+        (file, _) = ctd.split(',')
+        file = file.strip().replace('.asc', '.evl')
+        infn = inpath+'/'+file
+        evl_files.append(infn)
+    return evl_files
+
+def plotDepth(dt, depth, filepath):
     """
     Plots depth over time from .evl files
-    Input: x - Dates, y - depths, filepath - where to save the file
+    Input: datetimes, depths, filepath - where to save the file
     Output: Saves matplotlib of y vs x to filepath
     """
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1) 
-    plt.plot(x, y) # plot y vs x
+    plt.plot(dt, depth) # plot y vs x
     # Only have hour and minutes on x axis - samples usually taken on same day
     plt.gcf().autofmt_xdate()
     myFmt = mdates.DateFormatter('%H:%M')
     plt.gca().xaxis.set_major_formatter(myFmt)
 
-    plt.title('Depth over Time from ' + file)
+    plt.title('Depth over Time from ' + os.path.basename(filepath))
     plt.xlabel('Date and Time (h:m)')
     plt.ylabel('Depth from surface (m)')
      
@@ -39,47 +59,39 @@ def plotDepth(x, y, filepath):
 
 ### START HERE ###
 inpath = '/Volumes/GeringSSD/GU201905_CTD' # read in data - CHANGE TO MATCH LOCAL FILE STRUCTURE
-
 # Use CTDtoEVL list to plot - assumes that you just ran SeaBird-wTime-toEchoviewLine.py
-bfile = inpath+'/'+'CTDtoEVL.list'
-with open(bfile, 'r') as infile:
-    CTD_files = infile.readlines() # read in each CDT file name and the toffset
+ctd_list_file = inpath+'/'+'CTDtoEVL.list'
 
-for j in range(0, len(CTD_files)):
-    # Get the filename and replace .asc with .evl - you now have all .evl filenames
-    (file, _) = CTD_files[j].split(',')
-    file = file.strip().replace('.asc', '.evl')
-    infn = inpath+'/'+file
-    
-    # Read in each .evl file
-    with open(infn, 'r', encoding = "ISO-8859-1") as infile:
-        EVL_data = infile.readlines()
+def main():
+    evl_files = evl_list(ctd_list_file)
+    for file in evl_files:
+        # Read in each .evl file
+        with open(file, 'r', encoding = "ISO-8859-1") as infile:
+            evl_data = infile.readlines()
 
-    # Delete file headers
-    del EVL_data[0]
-    del EVL_data[1]
 
-    data = {}
-    for line in EVL_data[1:]:
-        line.rstrip()
-        # Pull out the date, time, and depth (one additional variable ignored) -  all of these are strings
-        (d, t, depth, tmpVars) = line.split(maxsplit=3)
-        dt = datetime.strptime(d+t, "%Y%m%d%H%M%S%f") # use date and time strings to make datetime object
-        data[dt] = -1*float(depth) # negate depth and make into float
+        depth_data = {}
+        for line in evl_data[2:]: # exclude the first two lines which are headers
+            line.rstrip()
+            # Pull out the date, time, and depth (one additional variable ignored) -  all of these are strings
+            (d, t, depth, tmpVars) = line.split(maxsplit=3)
+            dt = datetime.strptime(d+t, "%Y%m%d%H%M%S%f") # use date and time strings to make datetime object
+            depth_data[dt] = -1*float(depth) # negate depth and make into float
 
-    dt, depth = zip(*data.items()) # make date/time list and depth list from tuples 
-    outfn = infn.replace('.evl', '.png')
-    plotDepth(dt, depth, outfn)
-    
-    # If we have a max depth greater than 0, we have some bad data - this would be taking measurements above water
-    # Remove the 'bad' points and plot without them
-    if max(depth) > 0:
-        # remove data that a depth greater than 0
-        clean_dt,clean_depth= zip(*[(dt, depth) for dt, depth in data.items() if depth < 0])
-        clean_outfn = inpath+'/CLEAN_'+file.replace('.evl', '.png')
-        plotDepth(clean_dt, clean_depth, clean_outfn)
+        dt, depth = zip(*depth_data.items()) # make date/time list and depth list from tuples 
+        outfn = file.replace('.evl', ".png")
+        plotDepth(dt, depth, outfn)
 
-    
+        # If we have a max depth greater than 0, we have some bad data - this would be taking measurements above water
+        # Remove the 'bad' points and plot without them
+        if max(depth) > 0:
+            # remove data that a depth greater than 0
+            clean_dt,clean_depth= zip(*[(dt, depth) for dt, depth in depth_data.items() if depth < 0])
+            clean_outfn = file.replace('.evl', "") + "_CLEAN.png"
+            plotDepth(clean_dt, clean_depth, clean_outfn)
+
+if __name__ == "__main__":
+    main()
 
 
     
