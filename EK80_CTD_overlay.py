d7b1e212b90cf6cb4d5d169f47ac0e1f84a9623b
Skylar Gering <sgering@g.hmc.edu>
2021-06-15 09:04:33 -0700

    overlay plotting now has 7.5m offset and makes 4x4 grid of frequencies per echogram/CTD pairing

--- EK80_CTD_overlay.py
+++ EK80_CTD_overlay.py
@@ -20,7 +20,7 @@ from datetime import datetime
 import numpy as np
 from echolab2.instruments.util.simrad_calibration import calibration
 
-def echo_plot(ek, fig, frequency, form):
+def echo_plot(ek, fig, frequency, form, echo_bottom, x_lims, lower_threshold = -90):
     """
     Creates an echogram with a depth profile from CTD data overlayed over time
     Inputs: EK object (EK60 or EK80 - only EK80 tested), matplotlib figure
@@ -28,6 +28,9 @@ def echo_plot(ek, fig, frequency, form):
             if you want Sv data or Power data plotted
     Outputs: An echogram object
     """
+    fig.set_title(str(frequency) + "Hz")
+    fig.set_ylim(echo_bottom, 7.5)
+    fig.set_xlim(x_lims[0], x_lims[1])
     raw_data_list = ek.get_channel_data(frequencies=frequency) # get data from specific frequency
     raw_data = raw_data_list[frequency][0]
 
@@ -36,21 +39,23 @@ def echo_plot(ek, fig, frequency, form):
         # if there is more than one .raw file, this appends all data of specified frequency
         raw_data.append(file)
 
-    if form == "Pw":
+    if form == "Pw": # plot power data
         processed_power = raw_data.get_power() # get power data
         return echogram.Echogram(fig, processed_power)
 
-    if form == "Sv":
+    if form == "Sv": # plot Sv data
         cal_obj = raw_data.get_calibration()
         Sv = raw_data.get_Sv(calibation=cal_obj, return_depth=False) # get Sv data
-        Sv.transducer_draft = np.full(len(Sv.ping_time), 7.5)
+        Sv.transducer_draft = np.full(len(Sv.ping_time), 7.5) # sets transducer depth to 7.5 manually - might be wrong
         Sv.to_depth(cal_obj)
-        return echogram.Echogram(fig, Sv, threshold=[-90,-20])
+        return echogram.Echogram(fig, Sv, threshold=[lower_threshold,-20])
 
-# paths and files needed -  this script also needs 
+# paths and files needed
 ctd_path = '/Volumes/GeringSSD/GU201905_CTD/'
 raw_path = '/Volumes/GeringSSD/GU1905_Acoustic/EK60/'
 evl_raw_file = ctd_path + "/evl_raw_matches.list"
+# lower frequency threshold for noise
+lower_threshold = -70
 
 # Read in lines of evl_raw_matches.list 
 evl_raw_dic={}
@@ -58,23 +63,18 @@ with open(evl_raw_file, 'r') as file_matches:
         evl_raw = file_matches.readlines()
 
 # Make dictionary with evl file keys and raw file values
-for rows in evl_raw[1:2]: # should be 1 just for testing
+for rows in evl_raw[1:7]: # should be 1 just for testing
     evl, raw = rows.split(maxsplit=1)
     evl_raw_dic[evl] = raw.split()
-    
 ek80 = EK80.EK80()
 #print(vars(ek80))
-
 
 for ctd in evl_raw_dic:
     # RAW FILES
     raw_infiles = []
     for raw in evl_raw_dic[ctd]:
         raw_infiles.append(raw_path + raw)
-
-    fig, axs = plt.subplots()
     ek80.read_raw(raw_infiles)
-    echogram38 = echo_plot(ek80, fig, 38000, "Sv")
 
     #EVL FILES
     with open(ctd_path + ctd, 'r') as evl_infile:
@@ -94,16 +94,19 @@ for ctd in evl_raw_dic:
     for i in range(n_pings):
         dt_arr[i] = np.datetime64(dt[i])
 
-    depth_line = line.line(ping_time = dt_arr, data = depth_arr)
-    echogram38.plot_line(depth_line, linewidth=2.5, color = "black")
 
-    # PLOTTING AESTHETICS
-    plt.title("Sv data with CTD depth in time order for " + ctd)
-    axes = plt.gca()
-    axes.set_ylim(bottom = max(depth) + 10)
-    axes.set_xlim(min(dt_arr).astype('float'), max(dt_arr).astype('float'))
-    echogram38.add_colorbar(fig)
-    show()
-    #plt.savefig(ctd_path + ctd +"Echogram.png")
+    # PLOTTING
+    fig, axs = plt.subplots(2,2, figsize=(10,10))
+    fig.suptitle("Sv data with CTD depth in time order for " + ctd)
+    echo_bottom = max(depth) + 10
+    x_lim = (min(dt_arr).astype('float'), max(dt_arr).astype('float'))
+
+    fq_ax = [(18000, axs[0,0]), (38000, axs[0,1]), (120000, axs[1,0]), (200000, axs[1,1])]
+    # Plot 4x4 grid of each frequency for a CTD/echogram pairing
+    for fq in fq_ax:
+        echo_fq = echo_plot(ek80, fq[1], fq[0], "Sv", echo_bottom, x_lim, lower_threshold)
+        depth_line = line.line(ping_time = dt_arr, data = depth_arr)
+        echo_fq.plot_line(depth_line, linewidth=2.5, color = "black")
+    plt.savefig(ctd_path + ctd.replace(".evl", "") + "_echogram_" + str(lower_threshold) + ".png")
     plt.close()
 
