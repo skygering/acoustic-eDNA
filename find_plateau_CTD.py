97754400d13ca0d2d0c53801bf7e1239464466e6
Skylar Gering <sgering@g.hmc.edu>
2021-06-17 12:23:30 -0700

    basic segment seperation with lists

--- find_plateau_CTD.py
+++ find_plateau_CTD.py
@@ -4,24 +4,32 @@ import numpy as np
 
 x = np.array([*range(0,29)])
 y=np.array([*range(0,5)] + [*repeat(5,5)] + [*range(6,10)] +  [*repeat(10,5)] + [*range(9,-1, -1)])
-print(len(x))
-print(len(y))
+#print(len(x))
+#print(len(y))
 
-slopes =[]
-for i in range(len(x)-1):
-    s = (y[i+1] - y[i])/(x[i+1] - x[i])
-    slopes.append(s)
-slopes.append(slopes[-1]) # add on value for last element
-print(slopes)
+def calc_slope(x1, y1, x2, y2):
+    return (y2 - y1)/(x2 - x1)
 
-p1 = []
-p2 = []
-for j in range(len(slopes)):
-    if slopes[j] ==0 or slopes[j-1] == 0: # likely place for an error due to indexing
-        p1.append((x[j], y[j]))
+seg_num = 0
+segments = [[]]
+current_slope = calc_slope(x[0], y[0], x[1], y[1])
+for j in range(len(x)-1):
+    new_slope = calc_slope(x[j], y[j], x[j+1], y[j+1])
+    if new_slope != current_slope:
+        if current_slope == 0:
+            segments[seg_num].append((x[j], y[j]))
+            segments.append([])
+        else:
+            segments.append([(x[j], y[j])])
+        current_slope = new_slope
+        seg_num += 1
+    else:
+        segments[seg_num].append((x[j], y[j]))
+#print(segments)
 
-
-plt.plot(x,y)
-plt.plot(*zip(*p1))
+for segs in segments:
+    plt.plot(*zip(*segs))
+#plt.plot(x,y)
+#plt.plot(*zip(*p1))
 plt.show()
 plt.close()
