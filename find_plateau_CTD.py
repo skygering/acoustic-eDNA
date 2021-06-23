bec158c5c203d5849948989c9422d56d515f4a3b
Skylar Gering <sgering@g.hmc.edu>
2021-06-23 11:05:35 -0700

    further simplified plateau code by seperating segments by checking if they're water bottle samples only

--- find_plateau_CTD.py
+++ find_plateau_CTD.py
@@ -27,11 +27,11 @@ def calc_slope(x1, y1, x2, y2):
     '''
     return (y2 - y1)/(x2 - x1)
 
-def new_segment(segments, seg_num, slope):
+def new_segment(segments, seg_num, bottle):
     ''' 
     Creates a new segment - new entry in segment dictionary
     '''
-    segments[seg_num] = {'bottle': isclose(slope, 0, abs_tol= atol), "depth": math.nan, "usable": False, "points" : []}
+    segments[seg_num] = {'bottle': bottle, "depth": math.nan, "usable": False, "points" : []}
     return segments
 
 def to_float(x):
@@ -62,9 +62,9 @@ def plot_segments(segments):
 cast_dic = read_casts('/Volumes/GeringSSD/eDNA_cast.txt')
 
 
-casts = cast_dic[1] # which cast
-ctd = '/Volumes/GeringSSD/GU201905_CTD/ctd001.evl' # which CTD
-outfile_path = '/Volumes/GeringSSD/segments_cdt001.json'
+casts = cast_dic[17] # which cast
+ctd = '/Volumes/GeringSSD/GU201905_CTD/ctd017.evl' # which CTD
+outfile_path = '/Volumes/GeringSSD/segments_cdt017.json'
 depth_data = line.read_evl(ctd)
 x=depth_data.ping_time
 y=depth_data.data
@@ -74,28 +74,26 @@ x_hat = [num.astype("float") for num in x]
 y_hat = savgol_filter(y, 17, 1)
 
 # adjust for best results
-atol = .00017
+atol = .0003
 
 # setup for segments
 seg_num = 0
 segments = {}
 current_slope = calc_slope(x_hat[0], y_hat[0], x_hat[1], y_hat[1])
-segments = new_segment(segments, seg_num, current_slope)
+segments = new_segment(segments, seg_num, isclose(current_slope, 0, abs_tol= atol))
 
 for j in range(len(x)-1):
     x_j = np.datetime_as_string(x[j])
     y_j = float(y[j])
     new_slope = calc_slope(x_hat[j], y_hat[j], x_hat[j+1], y_hat[j+1])
-    if not math.isclose(new_slope, current_slope, abs_tol=atol):
-        if segments[seg_num]["bottle"] == isclose(new_slope, 0, abs_tol= atol):
-            segments[seg_num]["points"].append((x_j, y_j))
-        else:
-            seg_num += 1
-            segments = new_segment(segments, seg_num, new_slope) # makes empty new segment
-            segments[seg_num]["points"].append((x_j, y_j)) # new segment with point
-            current_slope = new_slope # approximate slope of new segment
+    new_bottle = isclose(new_slope, 0, abs_tol= atol)
+    if segments[seg_num]["bottle"] == new_bottle:
+        segments[seg_num]["points"].append((x_j, y_j))
     else:
-        segments[seg_num]["points"].append((x_j, y_j)) # puts point in current segment
+        seg_num += 1
+        segments = new_segment(segments, seg_num, new_bottle) # makes empty new segment
+        segments[seg_num]["points"].append((x_j, y_j)) # new segment with point
+        current_slope = new_slope # approximate slope of new segment
 segments[seg_num]["points"].append((np.datetime_as_string(x[-1]), float(y[-1]))) # add the last point to last segment
 
 
@@ -109,7 +107,10 @@ for num in range(len(segments)):
                 seg["usable"] = True 
 
 # Plotting
+print("Number of segments:")
+print(len(segments))
 plot_segments(segments)
+print("If number of segments is too high, try lowering atol.")
 
 with open(outfile_path, 'w') as outfile:
     json.dump(segments, outfile)
