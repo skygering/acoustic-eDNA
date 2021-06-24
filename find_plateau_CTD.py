1384c46076942c48e6bf889f64bfb5f3af226696
Skylar Gering <sgering@g.hmc.edu>
2021-06-23 18:11:23 -0700

    plateau code with equation for absolute difference value

--- find_plateau_CTD.py
+++ find_plateau_CTD.py
@@ -1,16 +1,17 @@
-from itertools import repeat
-from matplotlib import colors
 import matplotlib.pyplot as plt
 import numpy as np
 import json
 import math
 from math import isclose
-from scipy.linalg.lapack import _check_work_float
+from numpy.ma import mod
 from scipy.signal import savgol_filter
 from echolab2.processing import line
 from statistics import mean
 
 def read_casts(infile):
+    '''
+    Creates a dictionary of casts that lists the depths of the eDNA samples
+    '''
     with open(infile, 'r') as cast_file:
         cast_data = cast_file.readlines()
     cast_dic = {}
@@ -61,41 +62,63 @@ def plot_segments(segments):
 # Read in depths at which eDNA measurments were taken for each cast
 cast_dic = read_casts('/Volumes/GeringSSD/eDNA_cast.txt')
 
-
-casts = cast_dic[17] # which cast
-ctd = '/Volumes/GeringSSD/GU201905_CTD/ctd017.evl' # which CTD
-outfile_path = '/Volumes/GeringSSD/segments_cdt017.json'
+casts = cast_dic[3] # which cast
+ctd = '/Volumes/GeringSSD/GU201905_CTD/ctd003.evl' # which CTD
+outfile_path = '/Volumes/GeringSSD/segments_cdt003.json'
 depth_data = line.read_evl(ctd)
+
 x=depth_data.ping_time
 y=depth_data.data
-
-
 x_hat = [num.astype("float") for num in x]
-y_hat = savgol_filter(y, 17, 1)
+window_len = int(len(x) * 0.05)
+
+y_hat = savgol_filter(y, 21, 1)
+
 
 # adjust for best results
-atol = .0003
+atol_zero = max(y)*3.16E-7 + 9.6E-5
+print("Absolute Total (atol):")
+print(round(atol_zero, 5))
 
 # setup for segments
 seg_num = 0
 segments = {}
 current_slope = calc_slope(x_hat[0], y_hat[0], x_hat[1], y_hat[1])
-segments = new_segment(segments, seg_num, isclose(current_slope, 0, abs_tol= atol))
+segments = new_segment(segments, seg_num, isclose(current_slope, 0, abs_tol= atol_zero))
 
 for j in range(len(x)-1):
     x_j = np.datetime_as_string(x[j])
     y_j = float(y[j])
     new_slope = calc_slope(x_hat[j], y_hat[j], x_hat[j+1], y_hat[j+1])
-    new_bottle = isclose(new_slope, 0, abs_tol= atol)
-    if segments[seg_num]["bottle"] == new_bottle:
-        segments[seg_num]["points"].append((x_j, y_j))
-    else:
-        seg_num += 1
-        segments = new_segment(segments, seg_num, new_bottle) # makes empty new segment
-        segments[seg_num]["points"].append((x_j, y_j)) # new segment with point
-        current_slope = new_slope # approximate slope of new segment
+    new_bottle = isclose(new_slope, 0, abs_tol= atol_zero)
+    if segments[seg_num]["bottle"] == new_bottle: # we are still on the same segment - be that plateau or otherwise
+        segments[seg_num]["points"].append((x_j, y_j)) # add in current point
+    else: # we have now switched to a new type of segment
+        if len(segments[seg_num]["points"]) > 5: # if the previous segment is more than 2 points long
+            seg_num += 1
+            segments = new_segment(segments, seg_num, new_bottle) # makes empty new segment
+            segments[seg_num]["points"].append((x_j, y_j)) # Adds current point to new segment
+        else: # short previous segment - due to noise (too short to be actual segment)
+            segments[seg_num-1]["points"] += segments[seg_num]["points"] # add glitch points to previous segment
+            segments[seg_num-1]["points"].append((x_j, y_j)) # add new point to previous segment 
+            # since we only switch segments when they are diff type, previous and current must be same type with "glitch" inbetween 
+            del segments[seg_num] # delete "glitch segment"
+            seg_num -= 1
 segments[seg_num]["points"].append((np.datetime_as_string(x[-1]), float(y[-1]))) # add the last point to last segment
 
+print("Number of segments with these parameters:")
+print(len(segments))
+
+print("Close out of the graph once you have determined if it has the correct number of segments.")
+plot_segments(segments)
+
+print("Did the graph look correct?")
+input1 = input()
+print(input1)
+
+atol_depth = 2
+print("Depth variance to find eDNA casts:")
+print(atol_depth)
 
 for num in range(len(segments)):
     seg = segments[num]
@@ -103,14 +126,13 @@ for num in range(len(segments)):
         times, depths = zip(*seg["points"])
         seg["depth"] = mean(depths)
         for cast_depth in casts:
-            if isclose(seg["depth"], cast_depth, abs_tol=2) and cast_depth >= 5:
-                seg["usable"] = True 
+            if isclose(seg["depth"], cast_depth, abs_tol=atol_depth) and cast_depth >= 5:
+                seg["usable"] = True
 
-# Plotting
-print("Number of segments:")
-print(len(segments))
+
+
 plot_segments(segments)
-print("If number of segments is too high, try lowering atol.")
+
 
 with open(outfile_path, 'w') as outfile:
     json.dump(segments, outfile)
